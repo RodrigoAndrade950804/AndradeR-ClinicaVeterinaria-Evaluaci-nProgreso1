448d5891b30651e43022f21e1ab2823661628040
Rodrigo Andrade <rodrigo.andrade@hotmail.com>
2025-05-01 11:33:07 -0500

    Se incorporo pestañas dueños de las mascotas y lista de mascotas en el layout con sus respectivos controladores tambien se agrego link rel para ajustar tamaño de imagenes

---
+++ AndradeR-ClinicaVeterinaria-EvaluaciónProgreso1/Migrations/20250501162245_RodrigoAndrade3.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AndradeR_ClinicaVeterinaria_EvaluaciónProgreso1.Migrations
+{
+    /// <inheritdoc />
+    public partial class RodrigoAndrade3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
